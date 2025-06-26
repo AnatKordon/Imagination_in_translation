14355ab81b8788f1addb747cb9b2886de809d3d2
AnatKordon <blabla1107@gmail.com>
2025-06-26 13:38:02 +0200

    corrected test_api_key file

--- test_api_key.py
+++ test_api_key.py
@@ -3,7 +3,7 @@
 #pip install requests python-dotenv
 #pip install python-dotenv
 #and create a file ".env" with our api key in this format: STABILITY_API_KEY= (and then api key without quotes)
-
+#set your ".env" file under .gitignore so that you won't share the secret api key!!!
 # from dotenv import load_dotenv
 import os
 
