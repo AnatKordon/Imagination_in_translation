4c1cef4c2f3641444ef8823bd8b39f8dcb5730dc
itaipeleg1 <itaipeleg1@mail.tau.ac.il>
2025-07-01 13:31:10 +0300

    update

--- ui/ui_prototype.py
+++ ui/ui_prototype.py
@@ -14,7 +14,7 @@ ROOT = Path(__file__).resolve().parent
 GT_DIR = ROOT.parent / "data" / "wilma_ground_truth" # folder with target images
 GEN_DIR = ROOT / "generated" # folder for generated images
 LOG_DIR = ROOT / "data" / "logs" # folder with CSV log files per user/session
-FALLBACK = ROOT / "gen_1.png"# TO BE REMOVED: dummy placeholder picture that is used instead of a generated one
+FALLBACK = ROOT / "mona_lisa_2.jpg" # TO BE REMOVED: dummy placeholder picture that is used instead of a generated one
 
 MAX_ATTEMPTS = 5 # Attempts to improve the description are limited to 5
 IMG_H = 260  # The height of images is limited to 260 px so the user doesn't need to scroll
